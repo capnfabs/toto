b9a86aa5aac7358b18d08f39f32a9fffbf71e0c0
Fabian Tamp <fabian.tamp@gmail.com>
2020-12-21 21:50:03 +0100

    Got to 140/200

--- normalizer/normalize.py
+++ normalizer/normalize.py
@@ -1,52 +1,92 @@
+import json
 import sys
-from typing import Tuple
+from typing import Any, Dict, Optional, Tuple
 
 import musicbrainzngs
-from pony.orm import db_session
+from pony.orm import db_session, sql_debug
 
 import models
 
 
 def normalize_artist_title(sp: models.SongPlay) -> Tuple[str, str]:
-    title = sp.title.lower()
-    artist = sp.artist.lower()
+    title = sp.title.lower().strip()
+    artist = sp.artist.lower().strip()
     if '(' in title:
         title: str
         # Intentionally chose first one because sometimes they have multiple
         # bits after, e.g. heroes (david bowie cover) (radio edit)
         cut_title = title[:title.index('(')]
-        #print(f"Trimming title '{title}' to '{cut_title}")
-        return cut_title, artist
+        return cut_title.strip(), artist
     return title, artist
 
 
-PRINT_ALTS = False
+PRINT_ALTS = True
 
 
 def process_item(sp: models.SongPlay) -> None:
-    title, artist = normalize_artist_title(sp)
-    query = models.MusicBrainzDetails.select(lambda deets: deets.searched_artist == artist and deets.searched_title == title)
+    query = models.MusicBrainzDetails.select(lambda deets: deets.searched_artist == sp.artist and deets.searched_title == sp.title)
     with db_session:
-        if not query.exists():
-            print('Hitting MB for', artist, '-', title)
-            data = musicbrainzngs.search_recordings(recording=title, artist=artist, limit=5)
-            #print(data)
-            guessed_track = data['recording-list'][0]
-            mbid = guessed_track['id']
-            check_url = f'https://musicbrainz.org/recording/{mbid}'
-            check_title = guessed_track['title']
-            check_artist = ' /plus/ '.join(artist['name'] for artist in guessed_track['artist-credit'] if isinstance(artist, dict))
-            print('Chose         ', check_artist, '-', check_title, '-->', check_url)
-            if PRINT_ALTS:
-                alts = data['recording-list'][1:]
-                if alts:
-                    print('from')
-                for track in alts:
-                    #print(track)
-                    check_title = track['title']
-                    check_artist = ' /plus/ '.join(
-                        artist['name'] for artist in track['artist-credit'] if isinstance(artist, dict))
-                    print('    - ', check_artist, '-', check_title, '-->', check_url)
+        if query.exists():
+            return
+
+    print(f"Looking for '{sp.artist}' - '{sp.title}'")
+    # This adds to the database too.
+    find_candidate(sp)
+    pass
+
+
+MbRecording = Dict[str, Any]
+
+
+def songplay_matches(title: str, artist: str, recording: MbRecording) -> bool:
+    check_title = recording['title']
+    # artists are either an object or a string '/'.
+    artist_names = [artist['name'] for artist in recording['artist-credit'] if
+                    isinstance(artist, dict)]
+    first_artist = artist_names[0]
+    artist_match = artist.lower().startswith(first_artist.lower())
+    title_match = title.lower() == check_title.lower()
+    print('Artist match:', artist_match)
+    print('Title  match:', title_match)
+    return artist_match and title_match
+
+
+def format_recording(recording: MbRecording) -> str:
+    artist_names = [artist['name'] for artist in recording['artist-credit'] if
+                    isinstance(artist, dict)]
+    check_artist = ' /plus/ '.join(artist_names)
+    check_title = recording['title']
+    mbid = recording['id']
+    check_url = f'https://musicbrainz.org/recording/{mbid}'
+    return f"'{check_artist}' - '{check_title}' --> {check_url}"
+
+
+def find_candidate(sp: models.SongPlay) -> Optional[models.MusicBrainzDetails]:
+    title, artist = normalize_artist_title(sp)
+    print(f"Hitting MB for '{artist}' - '{title}'")
+    data = musicbrainzngs.search_recordings(recording=title, artist=artist, limit=5)
+    guessed_track: MbRecording = data['recording-list'][0]
+
+    if songplay_matches(title, artist, guessed_track):
+        mbid = guessed_track['id']
+        print('Chose         ', format_recording(guessed_track))
+        with db_session:
+            return models.MusicBrainzDetails(
+                searched_title=sp.title,
+                searched_artist=sp.artist,
+                musicbrainz_id=mbid,
+                musicbrainz_json=json.dumps(guessed_track),
+            )
+
+    if PRINT_ALTS:
+        print("Didn't find a good option, all candidates:")
+        alts = data['recording-list']
+        for track in alts:
+            print('    - ', format_recording(track))
+    else:
+        print("Didn't find a good options, skipping")
+
+    return None
 
 
 def main():
@@ -55,9 +95,9 @@ def main():
     models.connect_db(dbfile)
 
     with db_session:
-        all_songplays = models.SongPlay.select()[:100]
-        for sp in all_songplays:
-            process_item(sp)
+        all_songplays = models.SongPlay.select()[:200]
+    for sp in all_songplays:
+        process_item(sp)
 
 
 if __name__ == '__main__':
